c585ae43eec53d25a9e8d2529d12eca3d3d91e7a
John Olafenwa <john.olafenwa@microsoft.com>
2021-02-13 20:00:19 +0100

    fixed preprocessing issue in object detection

--- intelligencelayer/shared/utils/datasets.py
+++ intelligencelayer/shared/utils/datasets.py
@@ -859,6 +859,7 @@ def replicate(img, labels):
     return img, labels
 
 
+
 def letterbox(
     img,
     new_shape=(640, 640),
@@ -895,12 +896,12 @@ def letterbox(
 
     if shape[::-1] != new_unpad:  # resize
         # img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-        img = img.resize(new_unpad, resample=Image.BILINEAR)
+        img = img.resize(new_unpad, resample=Image.LINEAR)
 
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
     # img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    img = ImageOps.expand(img, (left, top, right, bottom))
+    img = ImageOps.expand(img, (left, top, right, bottom),114)
 
     return img, ratio, (dw, dh)
 
