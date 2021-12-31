8eb52337cfba8c07ad6c2b2f0a761f35357df3f7
OlafenwaMoses <guymodscientist@gmail.com>
2021-12-31 22:05:52 +0000

    update downalod links for dependencies

--- download_dependencies.ps1
+++ download_dependencies.ps1
@@ -6,9 +6,9 @@ function Download($URL, $NAME){
 
 }
 
-Download -URL "https://deepstack.blob.core.windows.net/shared-files/sharedfiles.zip" -NAME "sharedfiles"
-Download -URL "https://deepstack.blob.core.windows.net/shared-files/interpreter.zip" -NAME "interpreter"
-Download -URL "https://deepstack.blob.core.windows.net/shared-files/redis.zip" -NAME "redis"
-Download -URL "https://deepstack.blob.core.windows.net/shared-files/windows_packages_cpu.zip" -NAME "windows_packages_cpu"
-Download -URL "https://deepstack.blob.core.windows.net/shared-files/windows_packages_gpu.zip" -NAME "windows_packages_gpu"
-Download -URL "https://deepstack.blob.core.windows.net/shared-files/windows_setup.zip" -NAME "windows_setup"
+Download -URL "https://deepquest.sfo2.digitaloceanspaces.com/deepstack/shared-files/sharedfiles.zip" -NAME "sharedfiles"
+Download -URL "https://deepquest.sfo2.digitaloceanspaces.com/deepstack/shared-files/interpreter.zip" -NAME "interpreter"
+Download -URL "https://deepquest.sfo2.digitaloceanspaces.com/deepstack/shared-files/redis.zip" -NAME "redis"
+Download -URL "https://deepquest.sfo2.digitaloceanspaces.com/deepstack/shared-files/windows_packages_cpu.zip" -NAME "windows_packages_cpu"
+Download -URL "https://deepquest.sfo2.digitaloceanspaces.com/deepstack/shared-files/windows_packages_gpu.zip" -NAME "windows_packages_gpu"
+Download -URL "https://deepquest.sfo2.digitaloceanspaces.com/deepstack/shared-files/windows_setup.zip" -NAME "windows_setup"
